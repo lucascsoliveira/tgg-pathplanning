5e3eabfa1ad7f7d6f1e5e19616c1cc422391c428
Lucas Coutinho de S. Oliveira <lucascs.oliveira@gmail.com>
2017-12-20 16:37:29 -0200

    Create CarState

---
+++ src/main/java/io/github/lucascsoliveira/pathplanning/util/CarState.java
@@ -0,0 +1,76 @@
+package io.github.lucascsoliveira.pathplanning.util;
+
+public class CarState {
+
+	private DoubleOrderedPair position;
+	private Double theta;
+
+	public CarState() {
+		super();
+	}
+
+	public CarState(Double x, Double y, Double theta) {
+		super();
+		this.position = new DoubleOrderedPair(x, y);
+		this.theta = theta;
+	}
+
+	public CarState(IntegerOrderedPair discrete, DoubleOrderedPair continuous, Double theta) {
+		super();
+		this.position = continuous;
+		this.theta = theta;
+	}
+
+	public DoubleOrderedPair getContinuous() {
+		return position;
+	}
+
+	public void setContinuous(DoubleOrderedPair continuous) {
+		this.position = continuous;
+	}
+
+	public Double getTheta() {
+		return theta;
+	}
+
+	public void setTheta(Double theta) {
+		this.theta = theta;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((position == null) ? 0 : position.hashCode());
+		result = prime * result + ((theta == null) ? 0 : theta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CarState other = (CarState) obj;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (theta == null) {
+			if (other.theta != null)
+				return false;
+		} else if (!theta.equals(other.theta))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "State [position=" + position + ", theta=" + theta + "]";
+	}
+
+}
